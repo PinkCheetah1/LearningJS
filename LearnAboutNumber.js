3db4739bf1dc48f7de8bbfc7156c6a1ee936d51f
PinkCheetah1 <66535303+PinkCheetah1@users.noreply.github.com>
2025-03-26 15:21:23 -0600

    Adjusted for HTML Knowledge

---
+++ LearnAboutNumber.js
@@ -0,0 +1,18 @@
+/*
+This is a program and stuff
+*/
+
+
+// This is some old code I was trying to use to avoid using HTML
+// However, I've since learned HTML and will use it for this module. 
+// // ChatGPT told me this is an important line to set up so that Node.js
+// // can get user input. It's like setting up an input() function
+// const readline = require("readline");
+
+// // ChatGPT also told me this creates an input/output interface using the 
+// // readline we just created. 
+// // This will let me get my user input lol
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
